a6b592835c3b98d676497ac3d7cab8d1465444c3
Sino0on <dastan.kubanuulu1@gmail.com>
2022-08-03 16:28:51 +0600

    das

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os, sys
 sys.path.insert(0, '/var/www/u0000006/data/www/blandgroup.ru/blend')
-sys.path.insert(1, '/var/www/u0000006/data/djangoenv/lib/python3.7/site-packages')
+sys.path.insert(1, '/var/www/djangoenv/lib/python3.7/site-packages')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'blend.settings'
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
