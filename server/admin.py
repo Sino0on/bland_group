915fa09568c65c0aba4e1e89a56ee87d1f9e1f22
Sino0on <dastan.kubanuulu1@gmail.com>
2022-08-22 14:56:09 +0600

    add ckeditor

--- server/admin.py
+++ server/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
+from django import forms
+
 from .models import *
+from ckeditor.widgets import CKEditorWidget
+
+
+class NewsAdminForm(forms.ModelForm):
+    description = forms.CharField(widget=CKEditorWidget())
+
+    class Meta:
+        model = News
+        fields = '__all__'
+
+
+class NewsAdmin(admin.ModelAdmin):
+    form = NewsAdminForm
 
 admin.site.register(Product)
 admin.site.register(Image_product)
@@ -7,7 +22,7 @@ admin.site.register(Category)
 admin.site.register(Subcategory)
 admin.site.register(Work)
 admin.site.register(Image_work)
-admin.site.register(News)
+admin.site.register(News, NewsAdmin)
 admin.site.register(Image_news)
 admin.site.register(FAQ)
 admin.site.register(Feedback)
